5832c58b0a3ecf779f785576074346e0776e4ac3
edurs2602 <edurs.2602@gmail.com>
2023-04-18 12:20:59 -0300

    creating path and folders

---
+++ include/musica.h
@@ -0,0 +1,25 @@
+#include <string>
+
+#ifndef MUSICA_H
+#define MUSICA_H
+
+using namespace std;
+
+class Musica{
+  private:
+    string titulo;
+    string artista;
+
+  public:
+    Musica();
+    Musica(string titulo, string artista);
+
+};
+    
+#endif // !
+#define MUSICA_H
+
+
+
+
+
